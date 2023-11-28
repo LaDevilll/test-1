5c1a5b032606212059d8dc58b701527cb6d888ec
ARTEM K <tovkus.artem@mail.ru>
2023-11-29 00:33:04 +0700

    2 задача

---
+++ UnitTestProject2/UnitTest2.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using zadacha2;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod21()
+        {
+            double a = 1;
+            double b = -3;
+            double c = 2;
+            double[] result = Class1.Naidi2(a, b, c);
+            CollectionAssert.AreEqual(new double[] { 2, 1 }, result, Comparer<double>.Default);
+        }
+
+        [TestMethod]
+        public void TestMethod22()
+        {
+            double a = 1;
+            double b = -2;
+            double c = 1;
+            double[] result = Class1.Naidi2(a, b, c);
+            CollectionAssert.AreEqual(new double[] { 1 }, result, Comparer<double>.Default);
+        }
+
+        [TestMethod]
+        public void TestMethod23()
+        {
+            double a = 1;
+            double b = 2;
+            double c = 5;
+            double[] result = Class1.Naidi2(a, b, c);
+            CollectionAssert.AreEqual(new double[0], result, Comparer<double>.Default);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestMethod24()
+        {
+            double a = 0;
+            double b = 2;
+            double c = 3;
+            double[] result = Class1.Naidi2(a, b, c);
+        }
+    }
+}
---
+++ zadacha2/Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha2
+{
+    public class Class1
+    {
+        public static double[] Naidi2(double a, double b, double c)
+        {
+            if (Math.Abs(a) < 0.01)
+            {
+                throw new ArgumentException("Коэффициент а не может быть равен нулю");
+            }
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant > 0)
+            {
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                return new double[] { root1, root2 };
+            }
+            else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+                return new double[] { root };
+            }
+            else
+            {
+                return new double[0]; 
+            }
+        }
+    }
+}
