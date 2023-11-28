ba205b3f6d7049979d70380b5efbc07af917c33b
ARTEM K <tovkus.artem@mail.ru>
2023-11-28 23:32:02 +0700

    Добавьте файлы проекта.

---
+++ 1.1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    public class NNN
+    {
+        public static string Naidi(int n)
+        {
+            if (n < 1 || n > 26)
+            {
+                throw new ArgumentException("N дожно быть от 1 до 26");
+            }
+            char a = 'A';
+            return new string(Enumerable.Range(a, n).Select(c => (char)c).ToArray());
+        }
+    }
+}
---
+++ UnitTestProject1.1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using _1._1;
+
+namespace UnitTestProject1._1
+{
+    [TestClass]
+    public class NNNTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int n = 5;
+            string result = NNN.Naidi(n);
+            Assert.AreEqual("ABCDE", result);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestMethod12()
+        {
+            int n = 27;
+            string result = NNN.Naidi(n);
+        }
+    }
+}
